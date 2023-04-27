c1786b4ea7453f58b65545660ba91e86301e5a0c
Deyse Martellet <114524231+deysemartellet@users.noreply.github.com>
2023-04-27 20:43:29 -0300

    aula 9
    exercícios 2 e 3

--- Aula 9 2.cpp
+++ Aula 9 2.cpp
@@ -1,1 +1,16 @@
 // Escrever um programa que leia um número inteiro até que a soma de todos os números digitados for maior que 500.
+
+#include <stdio.h>
+
+int nro, soma;
+
+main(){
+    do {
+        printf("Digite um numero inteiro, positivo ou negativo: ");
+        scanf("%i", &nro);
+        soma+=nro;
+    } while (soma<=500);
+
+    printf("A soma chegou em %i", soma);
+}
+
--- Aula 9 3.cpp
+++ Aula 9 3.cpp
@@ -1,1 +1,19 @@
-// Escrever um programa que leia o salário inicial e faça aumentos sucessivos de 10% até que o valor do salário atualizado seja maior que o dobro do salário inicial.
+/* Escrever um programa que leia o salário inicial e faça aumentos sucessivos de 10% até que o valor do salário atualizado seja maior que o dobro do salário inicial. */
+
+#include <stdio.h>
+
+float sal_inicial, sal_aumento;
+int i;
+
+main(){
+        printf("Digite o salario inicial: ");
+        scanf("%f", &sal_inicial);
+        sal_aumento=sal_inicial;
+        do {
+            i++;
+            sal_aumento*=1.1;
+            printf("\n Atualizando 10%% %ix = %.2f ", i, sal_aumento); /* coloca %% pro programa saber que não é uma string e sim 10% mesmo*/
+        } while (sal_aumento<=sal_inicial*2);
+
+    printf("O salario final ficou em %.2f", sal_aumento);
+}
